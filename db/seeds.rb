ce581d1acf884f319957e086190d064ba812de73
sdempsey13 <stevendempsey13@gmail.com>
2019-07-23 12:34:37 -0400

    Updated portfolio to include attr for position.

--- db/seeds.rb
+++ db/seeds.rb
@@ -41,7 +41,7 @@ end
     subtitle: "Angular",
     body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat",
     main_image: "https://via.placeholder.com/600x400",
-    thumb_image: "https://via.placeholder.com/200",
+    thumb_image: "https://via.placeholder.com/350x200",
   )
 end
 
